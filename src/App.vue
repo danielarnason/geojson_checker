7127544b685eef871b2c5ae6c328e3f5b549ff37
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-08-29 12:26:39 +0200

    Leaflet med test geojson virker

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="cell cell-map"><MapContainer/></div>
+    <div class="cell cell-map"><MapContainer :geojson="this.geojson"/></div>
     <div class="cell cell-json"></div>
   </div>
 </template>
@@ -11,6 +11,46 @@ export default {
   name: 'App',
   components: {
     MapContainer
+  },
+  data () {
+    return {
+      geojson: {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [
+                    -26.982421875,
+                    66.26685631430843
+                  ],
+                  [
+                    -23.291015625,
+                    62.3903694381427
+                  ],
+                  [
+                    -11.25,
+                    63.11463763252091
+                  ],
+                  [
+                    -11.513671874999998,
+                    67.30597574414466
+                  ],
+                  [
+                    -26.982421875,
+                    66.26685631430843
+                  ]
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    }
   }
 }
 </script>
--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -3,11 +3,43 @@
 </template>
 
 <script>
-export default {
+import "leaflet/dist/leaflet.css"
+import L from 'leaflet'
 
+export default {
+    name: 'MapContainer',
+    data () {
+        return {
+            map: null,
+        }
+    },
+    props: [
+        'geojson'
+    ],
+    mounted: function () {
+        let ref = this;
+
+        let geojsonLyr = L.geoJSON(ref.geojson);
+        
+        ref.map = L.map('mapcontainer', {
+            center: [56.2, 9.5],
+            zoom: 4,
+            layers: [
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              }),
+              geojsonLyr
+            ]
+        })
+    }
 }
 </script>
 
 <style>
-
+#mapcontainer {
+    height: 100%;
+    width: 100%;
+    border-radius: 5px;
+    
+}
 </style>
